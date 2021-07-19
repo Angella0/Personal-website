deccb385c002de1e4eac9cf9d400d59ed7ba84e7
Angella0 <angellasimbwa@gmail.com>
2021-07-19 15:32:49 +0300

    Angella's portfolio

--- app.js
+++ app.js
@@ -1,40 +1,10 @@
-// 
-const TypeWriter = function(txtElement, words, wait = 3000){
-    this.txtElement = txtElement;
-    this.words = words;
-    this.txt = '';
-    this.wordIndex = 0;
-    this.wait = parseInt(wait, 10);
-    this.type();
-    this.isDeleting = false;
-}
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $(` .navbar`).addClass("sticky");
+        }else{
+            $(` .navbar`).removeClass("sticky");
+        }
 
-TypeWriter.protoype.type = function(){
-    const current =this.wordIndex % this.words.length;
-
-    const fullTxt = this.words[current];
-
-    if(this.isDeleting){
-
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-    }else{
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-
-    }
-    this.txtElement.innerHTML = `<span class = "txt"> ${this.txt}</span>`;
-
-
-    setTimeout(()=> this.type(), 500)
-}
-
-document.addEventListener('DOMContentLoaded', init);
-
-function init(){
-    const txtElement = document.querySelector('.text-type');
-    const words = JSON.parse(txtElement.getAttribute('data-words'));
-    const wait = txtElement.getAttribute('data-wait');
-
-    new TypeWriter(txtElement, words, wait);
-
-}
-
+    })
+});
