7ca36593d402d4b192dda139caf48c47f0b004b5
Angella0 <angellasimbwa@gmail.com>
2021-08-20 19:23:58 +0300

    commit

--- app.js
+++ app.js
@@ -9,3 +9,16 @@ $(document).ready(function(){
     })
     $
 });
+var typed = new Typed(".typing", {
+    strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+var typed = new Typed(".typing-2", {
+    strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
